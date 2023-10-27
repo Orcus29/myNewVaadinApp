b1a29893b522acf971fe85b38a5113a7aff45708
ns887 <axel.jean@partnre.com>
2023-10-27 04:02:54 +0200

    pom

--- src/main/java/com/bezkoder/spring/datajpa/ServletInitializer.java
+++ src/main/java/com/bezkoder/spring/datajpa/ServletInitializer.java
@@ -1,13 +1,15 @@
 package com.bezkoder.spring.datajpa;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootDataJpaApplication.class);
+		return application.sources(ServletInitializer.class);
 	}
 
 }
